c139c3140f5b2afd25a5d1341a387d572c9d86c7
Thibault <thibault.rieul@gmail.com>
2020-11-12 12:50:02 +0100

    Calcule vecteur Rmom

--- Momentum.py
+++ Momentum.py
@@ -27,22 +27,33 @@ def contribution_matrice(mom_data,rendement_data,prct_top,prct_flop,int_rw):
     mom_flop=0
     test_poids=0
     var_mom_temp=[]
-    for i in range(len(mom_data)):
+    Contr_mat=list(mom_data)
+    for i in range(len(Contr_mat)):
         test_poids=0
-        var_mom_temp=list(mom_data[i])
+        var_mom_temp=list(Contr_mat[i])
     
         mom_top=np.percentile(var_mom_temp,1-prct_top)
         mom_flop=np.percentile(var_mom_temp,prct_flop)
 
-        for k in range(len(mom_data[0])):
+        for k in range(len(Contr_mat[0])):
             if(var_mom_temp(k)<mom_top and var_mom_temp[k]>mom_flop):
                 var_mom_temp[k]=0
             
             elif(var_mom_temp[k]>=mom_top):
-                var_mom_temp[k]=1/(round(prct_top*len(mom_data[0]))*rendement_data[i+int_rw-1][k])
-                #test_poids=test_poids+1/(round(prct_top*len(mom_data[0])))
+                var_mom_temp[k]=1/(round(prct_top*len(Contr_mat[0]))*rendement_data[i+int_rw-1][k])
+                #test_poids=test_poids+1/(round(prct_top*len(Contr_mat[0])))
             elif(var_mom_temp[k]<=mom_flop):
-                var_mom_temp[k]=-1/(round(prct_flop*len(mom_data[0]))*rendement_data[i+int_rw-1][k])
-                #test_poids=test_poids-1/(round(prct_flop*len(mom_data[0])))
-        mom_data[i]=list(var_mom_temp)
-    return(mom_data)
+                var_mom_temp[k]=-1/(round(prct_flop*len(Contr_mat[0]))*rendement_data[i+int_rw-1][k])
+                #test_poids=test_poids-1/(round(prct_flop*len(Contr_mat[0])))
+        Contr_mat[i]=list(var_mom_temp)
+    return(Contr_mat)
+
+def Rmom(contr_mat):
+    Somme=0
+    V_Rmom=[]
+    for i in range(len(contr_mat)):
+        Somme=0
+        for j in range(contr_mat[0]):
+            Somme=Somme+contr_mat[i][j]
+        V_Rmom.append(Somme)
+    return(V_Rmom)
